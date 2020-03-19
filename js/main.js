a63e2b55f117369e48e0eb407d90ad9c118fda99
Buu90 <55487031+Buu90@users.noreply.github.com>
2020-03-19 16:10:13 +0300

    Add files via upload
    fix

--- js/main.js
+++ js/main.js
@@ -1,19 +1,43 @@
 "use strict";
 
-var array = [ 5, 7, [ 4, [2], 8, [1,3], 2 ], [ 9, [] ], 1, 8 ];
 
-var counter = 0;
+function subArr(arr) {
+    var count = 0;
+    manyArr(arr);
+    function manyArr(arr) {
+    for (var i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+    manyArr(arr[i]);
+    } else {
+    count += arr[i];
+    }
+    }
+    }
+    return count;
+    }
+    console.log(subArr([ 5, 7, [ 4, [2], 8, [1,3], 2 ], [ 9, [] ], 1, 8 ]));
 
+// var array = [ 5, 7, [ 4, [2], 8, [1,3], 2 ], [ 9, [] ], 1, 8 ];
 
-function foo(array){
-   
-    array.forEach(function(value, index) {   
-        
-    Array.isArray(value) ? foo(value) : counter += value;         
+// var counter = 0;
+
+// function foo(array){
     
-    });
-
-    return counter;
-
-}
-console.log(foo(array));
+//     for (let i = 0; i < array.length; i++) {
+        
+//         if (Array.isArray(array[i])) {
+             
+//             foo(array[i]);
+        
+//         } else {
+            
+//             counter += array[i];
+            
+//         }
+        
+//     }
+    
+//     return counter;
+    
+// }
+// console.log(foo(array));
